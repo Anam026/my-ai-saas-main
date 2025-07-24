ff25ac3dc37ca9f39cbca29037b4acf2f5d67112
Anam Khanzadah <anam.khanzada26@gmail.com>
2025-07-24 22:20:55 +0530

    integrated sidebar into DashboardLayout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/navbar";
-//SIDEBAR
+//SIDEBAR AND NAVBAR
+import Sidebar from "@/components/sidebar";
 
 const DashboardLayout = ({
     children
@@ -10,9 +11,8 @@ const DashboardLayout = ({
             <div className="h-full relative">
                 <div className="hidden h-full md:flex
                 md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-                    <div>
-                        Hello Sidebar
-                    </div>
+                    <Sidebar />
+
                 </div>
                 <main className="md:pl-72">
                     <Navbar />
