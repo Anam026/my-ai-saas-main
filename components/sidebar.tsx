5be52629bb302a20895132e0dacbf5cdf131f98b
Anam Khanzadah <anam.khanzada26@gmail.com>
2025-07-26 00:46:12 +0530

    Implement dashboard layout

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -68,8 +68,8 @@ const routes = [
 
 const Sidebar = () => {
     const pathname = usePathname();
-    return(
-        <div className=" space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
+    return(  //sidebar ui
+        <div className=" space-y-4 py-4 flex flex-col h-full bg-[#0A0013] text-[#5A7FC8]">
             <div className="px-3 py-2 flex-1">
                 <Link href="/dashboard" className="flex items-center pl-3 mb-14">
                 <div className="relative w-8 h-8 mr-4">
