cb9a751f2ac078ef99915df2dcc6202b875b9ae1
Anam Khanzadah <anam.khanzada26@gmail.com>
2025-07-29 00:40:19 +0530

    Add Radix UI Select component

---
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -0,0 +1,140 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { ImageIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { Heading } from "@/components/heading";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { cn } from "@/lib/utils";
+import { Loader } from "@/components/loader";
+import { Empty } from "@/components/empty";
+
+import { formSchema } from "./constants";
+import { Select } from "@/components/ui/select";
+
+const ImagePage = () => {
+  const router = useRouter();
+  const [images, setImages] = useState<string[]>([]);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+      amount:"1",
+      resolution: "512x512"
+    }
+  });
+
+  const isLoading = form.formState.isSubmitting;
+  
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setImages([]);
+
+      const response = await axios.post('/api/image', values);
+
+      const urls = response.data.map((image: {url: string}) => image.url);
+
+
+      setImages(urls);
+      form.reset();
+    } catch (error: any) {
+      //TODO: Open Pro Modal
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return ( 
+    <div>
+      <Heading
+        title="Image Generation"
+        description="Turn your prompt into an Image."
+        icon={ImageIcon}
+        iconColor="text-pink-700"
+        bgColor="bg-pink-700/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form 
+              onSubmit={form.handleSubmit(onSubmit)} 
+              className="
+                rounded-lg 
+                border 
+                w-full 
+                p-4 
+                px-3 
+                md:px-6 
+                focus-within:shadow-sm
+                grid
+                grid-cols-12
+                gap-2
+              "
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading} 
+                        placeholder="A picture of a cat with a hat." 
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField 
+                control={form.control}
+                name="amount"
+                render={({field})=> (
+                  <FormItem className="col-span-12 lg:col-span-2">
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      defaultValue={field.value}
+                      >
+
+                    </Select>
+
+                  </FormItem>
+                )} 
+              />
+
+              <Button className="col-span-12 lg:col-span-2 w-full" type="submit" disabled={isLoading} size="icon">
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-20">
+              <Loader />
+            </div>
+          )}
+          {images.length === 0 && !isLoading && (
+            <Empty label="No images generated." />
+          )}
+          <div>
+            Images will be rendered here
+          </div>
+        </div>
+      </div>
+    </div>
+   );
+}
+ 
+export default ImagePage;
